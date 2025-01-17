54867225f9bb8ba1e0dd7c20f883c922bd8734a2
Shashi-Tej-Reddy-Singa-Reddy <shashitejreddysingareddy@gmail.com>
2025-01-17 23:24:23 +0530

    d

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar/Navbar'; // Import the Navbar component
+import Home from './Home/Home'; // Import pages (create these in the next steps)
+// import About from './pages/About';
+// import Services from './pages/Services';
+// import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/about" element={<About />} /> */}
+            {/* <Route path="/services" element={<Services />} /> */}
+            {/* <Route path="/contact" element={<Contact />} /> */}
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
