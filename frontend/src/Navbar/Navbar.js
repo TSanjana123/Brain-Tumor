75b523071193b22c2e826261337640aee988e843
Shashi-Tej-Reddy-Singa-Reddy <shashitejreddysingareddy@gmail.com>
2025-01-17 23:18:53 +0530

    jh

---
+++ frontend/src/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+// Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">Home</Link>
+        <ul className="navbar-links">
+          <li className="navbar-item">
+            <Link to="/about" className="navbar-link">About</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/services" className="navbar-link">Services</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/contact" className="navbar-link">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
