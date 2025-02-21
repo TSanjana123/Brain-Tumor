7698a0594006b7039abd6baad28626067bcae869
TSanjana123 <tsanjana2727@gmail.com>
2025-02-22 01:19:17 +0530

    o

--- frontend/src/Predict/Organization.js
+++ frontend/src/Predict/Organization.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import Organization from './Predict/Organization';
 import './Organization.css';
 
 const Organization = () => {
