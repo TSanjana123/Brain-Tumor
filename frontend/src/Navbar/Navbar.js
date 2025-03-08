8468b816e46f357d60473344a0a1f049f914f6b5
TSanjana123 <tsanjana2727@gmail.com>
2025-03-08 19:48:43 +0530

    d

--- frontend/src/Navbar/Navbar.js
+++ frontend/src/Navbar/Navbar.js
@@ -41,12 +41,12 @@ function Navbar() {
             <li className="nav-item">
               <Link to="/about" className="nav-link">About</Link>
             </li>
-            <li className="nav-item">
+            {/* <li className="nav-item">
               <Link to="/services" className="nav-link">Services</Link>
             </li>
             <li className="nav-item">
               <Link to="/contact" className="nav-link">Contact</Link>
-            </li>
+            </li> */}
             <li className="nav-item">
               <Link to="/Signup" className="nav-link">Signup</Link>
             </li>
@@ -61,9 +61,9 @@ function Navbar() {
             </li>
             <li className="nav-item">
               <Link to="/Test" className="nav-link">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-folder" viewBox="0 0 16 16">
+               {/* <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-folder" viewBox="0 0 16 16">
                   <path d="M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139q.323-.119.684-.12h5.396z" />
-                </svg>
+                </svg>  */}
               </Link>
             </li>
           </ul>
