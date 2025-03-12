69087e1e11257ccffc74b1a3513b2fd04b0e435e
Shashi-Tej-Reddy-Singa-Reddy <shashitejreddysingareddy@gmail.com>
2025-03-12 14:23:34 +0530

    z

--- frontend/src/Login/Login.js
+++ frontend/src/Login/Login.js
@@ -403,6 +403,8 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
+      console.log("shashi")
+      console.log(envprocess.env.REACT_LOGIN_RESPONSE_URL)
       // const response = await axios.post('http://localhost:5001/api/login', formData);
       const response = await axios.post(`${process.env.REACT_LOGIN_RESPONSE_URL}/api/login`, formData);
       const { name, email, role, token, patientId, organizationName } = response.data;
