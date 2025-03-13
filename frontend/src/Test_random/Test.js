f76f970d5939b2d25878a76fbfa8b174411a7f9d
Shashi-Tej-Reddy-Singa-Reddy <shashitejreddysingareddy@gmail.com>
2025-03-13 14:39:23 +0530

    s

--- frontend/src/Test_random/Test.js
+++ frontend/src/Test_random/Test.js
@@ -94,12 +94,6 @@ function Test() {
           <div class="carousel-item active">
             <img src="https://images.pexels.com/photos/2325447/pexels-photo-2325447.jpeg" class="d-block w-100" alt="..."/>
           </div>
-          <div class="carousel-item">
-            <img src="https://images.pexels.com/photos/2325447/pexels-photo-2325447.jpeg" class="d-block w-100" alt="..."/>
-          </div>
-          <div class="carousel-item">
-            <img src="https://images.pexels.com/photos/2325447/pexels-photo-2325447.jpeg" class="d-block w-100" alt="..."/>
-          </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
